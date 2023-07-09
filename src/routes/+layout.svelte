b69039553757e0b884bbaa0ffc18300731046dfc
Marco <marcosuarezp95@gmail.com>
2023-07-08 23:56:44 -0400

    set up skeleton ui

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+  import "../app.postcss";
+</script>
+
+<svelte:head>
+  <title>Cinemarc</title>
+</svelte:head>
+
+<slot />
+
+<style style="scss">
+  @import "../styles/styles.scss";
+</style>
