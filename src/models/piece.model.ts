6b0d91825014e66e1c92327a11b93e3618a09cf9
Marco <marcosuarezp95@gmail.com>
2023-07-09 10:31:52 -0400

    changes on piece model

--- src/models/piece.model.ts
+++ src/models/piece.model.ts
@@ -1,10 +1,10 @@
 import type { FirestoreDocument } from "./firestore.model";
 import type { Nullable } from "./general.model";
 
-export type PieceType = "movie" | "series" | "podcast" | "documentary" | "video";
+export type PieceType = "movie" | "series" | "podcast" | "documentary" | "video" | "book" | "music";
 
 export type PieceSource = "netflix" | "youtube" | "torrent_file" | "downloaded" | "hbo_max" | 
-                          "prime_video" | "theater";
+                          "prime_video" | "theater" | "physical";
 
 export interface PieceCreate {
   name: string;
