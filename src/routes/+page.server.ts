396bbad42624372fbff95eaab75cbd2587118f2c
Marco <marcosuarezp95@gmail.com>
2023-07-10 23:51:31 -0400

    remove bad import

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,5 +1,4 @@
 import { error } from '@sveltejs/kit';
-import { getPieces } from '../server/controllers/piece.controller';
 import { toSerializable } from '../server/utils.server';
 
 /** @type {import('./$types').PageServerLoad} */
