905a7304e59d419df693c3a5dee011981dbb773c
Marco <marcosuarezp95@gmail.com>
2023-07-10 23:13:13 -0400

    move fb serv acc to env

--- src/server/firebase.server.ts
+++ src/server/firebase.server.ts
@@ -1,9 +1,10 @@
 import admin from "firebase-admin";
 import type { DecodedIdToken } from "firebase-admin/lib/auth/token-verifier";
-import serviceAccount from "./service-account.json";
+import { FIREBASE_SERVICE_ACCOUNT } from "$env/static/private";
 
 export function initializeFirebase() {
   if (!admin.apps.length) {
+		const serviceAccount = JSON.parse(FIREBASE_SERVICE_ACCOUNT);
 		admin.initializeApp({
 			credential: admin.credential.cert(serviceAccount as any),
 			databaseURL: `https://${serviceAccount.project_id}.firebaseio.com`
