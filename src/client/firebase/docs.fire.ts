e4541b09c0933381f6c5ec664bb3b76bf6eb2949
Marco <marcosuarezp95@gmail.com>
2023-07-17 09:41:33 -0400

    imp helper types and fns for firebase

--- src/client/firebase/docs.fire.ts
+++ src/client/firebase/docs.fire.ts
@@ -1,6 +1,7 @@
-import type { DocumentData, QuerySnapshot } from "firebase/firestore";
+import { DocumentReference, onSnapshot, type DocumentData, type Query, type QuerySnapshot, type Unsubscribe } from "firebase/firestore";
+import type { ColObserverCompleteCallback, ColSnapshotCallback, ColSnapshotErrorCallback, ValueCallback } from "../../models/general.model";
 
-export function snapData<T>(snap: QuerySnapshot<DocumentData, DocumentData>): T[] {
+export function snapData<T>(snap: QuerySnapshot<T, DocumentData>): T[] {
   return snap.docs.map((doc) => doc.data() as T);
 }
 
@@ -13,4 +14,37 @@ export function generateId(): string {
       autoId += chars.charAt(Math.floor(Math.random() * chars.length));
     }
     return autoId;
+}
+
+export function subscribeTo<T = any>(
+  q: Query<T>, 
+  next: ColSnapshotCallback<T>, 
+  error?: ColSnapshotErrorCallback<T>, 
+  complete?: ColObserverCompleteCallback
+) {
+  return onSnapshot(q, { next, error, complete });
+}
+
+export function valueCollectionSnap<T = any>(
+  q: Query<T>, 
+  _next: ValueCallback<T[]>,
+): Unsubscribe {
+  return onSnapshot(q, {
+    next: (snap) => {
+      const data = snapData(snap);
+      _next(data);
+    }
+  })
+}
+
+export function valueDocSnap<T = any>(
+  q: DocumentReference<T>, 
+  _next: ValueCallback<T | undefined>,
+): Unsubscribe {
+  return onSnapshot(q, {
+    next: (snap) => {
+      const data = snap.data();
+      _next(data);
+    }
+  })
 }
--- src/models/general.model.ts
+++ src/models/general.model.ts
@@ -1,3 +1,5 @@
+import type { DocumentData, DocumentReference, FirestoreError, QuerySnapshot } from "firebase/firestore";
+
 export type Nullable<T> = T | null;
 
 export interface ServerTimeDoc<T> {
@@ -31,3 +33,25 @@ export interface Paged<T> {
   page: number;
   total: number;
 }
+
+export type ColSnapshotCallback<T> = (snapshot: QuerySnapshot<T, DocumentData>) => void;
+export type ColSnapshotErrorCallback<T> = (error: FirestoreError) => void;
+export type ColObserverCompleteCallback = () => void;
+
+export interface ColObserver<T> {
+  next?: ColSnapshotCallback<T>;
+  error?: ColSnapshotErrorCallback<T>;
+  complete?: ColObserverCompleteCallback;
+}
+
+export type DocSnapshotCallback<T> = (snapshot: DocumentReference<T, DocumentData>) => void;
+export type DocSnapshotErrorCallback<T> = (error: FirestoreError) => void;
+export type DocObserverCompleteCallback = () => void;
+
+export interface DocObserver<T> {
+  next?: DocSnapshotCallback<T>;
+  error?: DocSnapshotErrorCallback<T>;
+  complete?: DocObserverCompleteCallback;
+}
+
+export type ValueCallback<T> = (data: T) => void;
