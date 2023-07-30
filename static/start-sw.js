d0a465d7e330a6d703638a41286172b4e15aff3f
Marco <marcosuarezp95@gmail.com>
2023-07-30 17:09:14 -0400

    service worker try

---
+++ static/start-sw.js
@@ -0,0 +1,8 @@
+import { dev } from '$app/environment';
+if ('serviceWorker' in navigator) {
+  addEventListener('load', function () {
+    navigator.serviceWorker.register('../service-worker.js', {
+        type: dev ? 'module' : 'classic'
+    });
+  });
+}
