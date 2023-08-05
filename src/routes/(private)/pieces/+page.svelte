45db16ccd57f2ec174c40dd173ea953d5f1a96b8
Marco <marcosuarezp95@gmail.com>
2023-08-05 17:22:42 -0400

    move plus button and piece modal to pieces page

--- src/routes/(private)/pieces/+page.svelte
+++ src/routes/(private)/pieces/+page.svelte
@@ -50,7 +50,7 @@
     searchInputEl.value = "";
   }
 
-  function handleFormSuccess() {
+  function handleFormUpdateSuccess() {
     modalStore.close();
     const t: ToastSettings = {
       message: 'Piece updated! 🎉',
@@ -65,8 +65,9 @@
     const formModalComponent: ModalComponent = {
       ref: PieceForm,
       props: {
-        success: handleFormSuccess,
+        success: handleFormUpdateSuccess,
         pieceToEdit: piece,
+        close: onCloseModalClick,
       },
     }
     const formModal: ModalSettings = {
@@ -80,6 +81,32 @@
     signOut();
   }
 
+  function handleFormCreateSuccess() {
+    modalStore.close();
+    const t: ToastSettings = {
+      message: 'Piece created! 🎉',
+      background: 'variant-filled-success',
+    };
+    toastStore.trigger(t);
+  }
+  function onCloseModalClick() {
+    modalStore.close();
+  }
+
+  function handlePlusButtonClick() {
+    const formModalComponent: ModalComponent = {
+      ref: PieceForm,
+      props: {
+        success: handleFormCreateSuccess,
+        close: onCloseModalClick,
+      },
+    }
+    const formModal: ModalSettings = {
+      type: "component",
+      component: formModalComponent,
+    }
+    modalStore.trigger(formModal);
+  }
 </script>
 
 <style lang="scss">
@@ -138,6 +165,10 @@
     {#each displayedPieces as piece, index}
       <PieceCard on:editButtonClick={openEditModal} {piece} {index} />
     {/each}
+
+  <button on:click={handlePlusButtonClick} type="button" class="btn-icon btn-icon-xl variant-filled fixed bottom-6 right-6">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="#000" width="24" height="24" viewBox="0 0 24 24"><path d="M24 10h-10v-10h-4v10h-10v4h10v10h4v-10h10z"/></svg>
+  </button>
   {:else}
     <Spinner />
   {/if}
