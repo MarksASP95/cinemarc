22d4cc2ad46d2a12891d759cb5245e60b4659f3b
Marco <marcosuarezp95@gmail.com>
2023-08-15 16:59:24 -0400

    imp basic drawer

--- src/routes/(private)/+layout.svelte
+++ src/routes/(private)/+layout.svelte
@@ -1,12 +1,15 @@
 <style lang="scss">
-  .main-container {
+  @mixin general_layout {
     padding: 0 20px 20px;
     max-width: 1920px;
     margin: 0 auto;
-
     @media (min-width: 768px) {
       padding: 0 120px 40px;
     }
+  }
+
+  .main-container {
+    @include general_layout;
   }
 
   header.cinemarc-header {
@@ -19,6 +22,21 @@
       font-family: "Sono";
     }
   }
+
+  .drawer-content {
+    @include general_layout;
+    padding-top: 20px;
+    @media (min-width: 768px) {
+      padding-top: 40px;
+    }
+
+    .initial-text {
+      font-family: "Sono";
+    }
+    .username {
+      font-family: "Sono";
+    }
+  }
 </style>
 
 <script lang="ts">
@@ -26,6 +44,7 @@
   import { authUser$ } from '../../auth/auth.store';
   import type { CinemarcUser } from '../../models/user.model';
   import { signOut } from '../../client/firebase/auth.fire';
+  import { Drawer, drawerStore } from '@skeletonlabs/skeleton';
 
   let currentUser: CinemarcUser | undefined | null;
   onMount(() => {
@@ -37,7 +56,47 @@
   function handleSignOutClick() {
     signOut();
   }
+
+  function openDrawer() {
+    drawerStore.open({
+      position: "top",
+      height: "auto",
+    });
+  }
+
+  function getUsernameInitial(username: string): string {
+    return username[0];
+  }
 </script>
+
+{#if !!currentUser}
+  <Drawer>
+    <div class="drawer-content">
+      <div class="flex justify-center items-center flex-col pb-4">
+        <div class="rounded-full w-16 h-16 overflow-hidden flex justify-center items-center variant-glass-surface">
+          {#if currentUser.avatarUrl}
+            <img class="object-cover" src={currentUser.avatarUrl} alt={currentUser.username}>
+          {:else}
+            <p class="text-center initial-text text-2xl">
+              {getUsernameInitial(currentUser.username)}
+            </p>
+          {/if}
+        </div>
+
+        <p class="username">
+          {currentUser.username}
+        </p>
+        <!-- <Avatar src="" width="w-32" rounded="rounded-full" /> -->
+      </div>
+      <div class="flex justify-center mt-4">
+        <button on:click={handleSignOutClick} type="button" class="btn btn-sm bg-gradient-to-br variant-gradient-error-success">
+          <span>sign out</span>
+          <span>👋</span>
+        </button>
+      </div>
+    </div>
+  </Drawer>
+{/if}
 
 <main class="main-container">
   {#if currentUser}
@@ -45,9 +104,8 @@
       <h1 class="cinemarc-home">
         cinemarc <span class="font-mono">({ currentUser?.username || currentUser?.email || '' })</span>
       </h1>
-      <button on:click={handleSignOutClick} type="button" class="btn btn-sm bg-gradient-to-br variant-gradient-error-success">
-        <span>sign out</span>
-        <span>👋</span>
+      <button on:click={openDrawer} type="button" class="btn btn-sm bg-gradient-to-br variant-gradient-tertiary-secondary">
+        <span>⚙️</span>
       </button>
     </header>
     {:else}
@@ -58,7 +116,7 @@
         </h1>
         <span class="placeholder animate-pulse" style="height: 25px; width: 100px;"></span>
       </div>
-      <div class="placeholder animate-pulse" style="height: 32px; width: 100px;"></div>
+      <div class="placeholder animate-pulse" style="height: 32px; width: 40px;"></div>
     </header>
 {/if}
 
