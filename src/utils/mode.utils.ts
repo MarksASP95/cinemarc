1284cbfb2b76a0cb3951a10ad8522cc61a305517
Marco <marcosuarezp95@gmail.com>
2023-08-19 19:29:40 -0400

    movile top bar color change attempt

--- src/utils/mode.utils.ts
+++ src/utils/mode.utils.ts
@@ -1,6 +1,20 @@
-import { getModeUserPrefers, setModeCurrent, setModeUserPrefers } from "@skeletonlabs/skeleton";
+import { getModeUserPrefers, modeCurrent, setModeCurrent, setModeUserPrefers } from "@skeletonlabs/skeleton";
 
 export function initMode() {
+
+  modeCurrent.subscribe((isLight) => {
+    const color = isLight ? "#FAf8FC" : "#120B18";
+    const themeColorMetaTag = document.querySelector('meta[name="theme-color"]') as HTMLMetaElement;
+    if (!themeColorMetaTag) {
+      const metaTag = document.createElement("META") as HTMLMetaElement;
+      metaTag.name = "theme-color";
+      metaTag.content = color;
+      document.head.append(metaTag);
+    } else {
+      themeColorMetaTag.content = color;
+    }
+  })
+
   const userPrefers = getModeUserPrefers();
   if (userPrefers === undefined) {
     setModeCurrent(false);
