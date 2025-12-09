62ff8986447538cad1cb77380cc9d2a98e83c78f
Marco Suarez <marcosuarezp95@gmail.com>
2025-12-09 18:17:11 -0400

    try another fix

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,7 @@ const config = {
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter({
-      runtime: "nodejs24.x",
+      runtime: "vercel/nodejs@24.x",
     }),
   },
   preprocess: [vitePreprocess({})],
