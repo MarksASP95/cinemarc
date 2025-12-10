1c4b856c60c432038e33d3cf2981be454ab765be
Marco Suarez <marcosuarezp95@gmail.com>
2025-12-10 10:10:22 -0400

    try with runtime 18

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,8 @@ const config = {
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter({
-      runtime: "nodejs20.x",
+      // Vercel currently supports nodejs16.x and nodejs18.x runtimes; use 18.x.
+      runtime: "nodejs18.x",
     }),
   },
   preprocess: [vitePreprocess({})],
